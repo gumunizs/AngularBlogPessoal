cf8ee5087d2a73e32824757d539df79e23d5c43c
Gustavo Muniz <gustavomunizsantos@gmail.com>
2021-07-29 23:39:25 -0300

    Primeira parte Consumo de API

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+usuarioo: Usuario = new Usuario
+confirmarsenha: string
+tipousuario:string
+  constructor(private authService: AuthService,
+    private router: Router) { }
+  
+  ngOnInit() {
+    window.scroll(0,0)
 
-  ngOnInit(): void {
   }
+confirmsenha(event: any) {
+  this.confirmarsenha = event.target.value
 
+}
+
+tipoUser(event: any){
+this.tipousuario = event.target.value
+}
+cadastrarr(){
+  this.usuarioo.tipo = this.tipousuario
+  if(this.usuarioo.senha != this.confirmarsenha){
+    alert('A senhas estão incorretas')
+  } else{
+    this.authService.cadastrar(this.usuarioo).subscribe((resp: Usuario) => {
+      this.usuarioo = resp
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso')
+    })
+  }
+}
 }
---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,10 @@
+export class UsuarioLogin{
+    public id: number
+    public nome:	string
+    public senha:	string
+    public token:	string
+    public usuario:	string
+    public foto: string
+    public tipo: string
+
+}
